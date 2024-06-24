f416bd7f4abc965d81c1f9c36fa88dfc468de9d4
Harshitpapneja <140529258+Harshitpapneja@users.noreply.github.com>
2024-06-24 22:35:24 +0000

    conversion of dataypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,30 @@
+let score = "99"
+
+// const {score} = req.body
+console.log(typeof score);
+console.log(typeof (score));
+
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber); 
+ console.log(valueInNumber)
+// abc hoga no sath to value aegi not a number naN 
+// agr value null hoti to output atta 0 for matching temp 
+// agr value undefined hoti to output atta naN 
+// boolean values true false pr 1 0r 0 aega obvvv
+// or koi string hoga to bi naN mtlb name hoga to vo convert nhi ho skta
+// "33" => 33
+// "33abs" => NaN
+// true or false => 1 0r 0
+let isLoggedIn =1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1 or 0 => true and false;
+// "" => false 
+// "harshit"=> true
+let someNumber = 12
+
+let stringSomeNumber = String(someNumber)
+
+console.log(stringSomeNumber)
+console.log(typeof stringSomeNumber)
+
