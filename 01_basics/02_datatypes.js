93440520b49912679bd05102121b554fef0fc0f7
Harshitpapneja <140529258+Harshitpapneja@users.noreply.github.com>
2024-06-24 21:42:54 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict";
+// Treat all js code as newer version
+// alert( 3+3)
+// we are using using not browser alg syntax h iska krenge aage use 
+// semicolon avoid kr skte h
+console.log(3+5)
+console.log("harshit bhai h ")
+// code readibility should be high bekar me upr niche ni krna theek 
+// tc39 and mdn documentation kuch standards set krte h 
+let name = "harshit"
+let age = 18
+let isLoggedIn = false
+let value
+let state = null
+// number => 2 to the power of either 52 or 53
+// bigint trading
+// string => ""
+// boolean =>false or true 
+// null standalone value as well as dataype // empty 
+// undefined => value declare ni kri
+// symbol used for uniqueness 
+// these all are primitive datatypes 
+
+// object 
+console.log(typeof 18);
+// datatype ka type btaega 
+console.log(typeof "harshit");
+// console.log(typeof "undefined"); putput is undefined 
+
+// console.log(typeof "harshit"); output is object 
+// null ek object h 
