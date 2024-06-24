2bffb868dabc110f4d906d9a4082994d040f9915
Harshitpapneja <140529258+Harshitpapneja@users.noreply.github.com>
2024-06-24 01:53:10 +0000

    container config done

---
+++ 01_basics/01_variables and cons.js
@@ -0,0 +1,19 @@
+const accountId = 1445534
+let accountEmail = "harshit@gmail.com"
+var accountPassword = "12234"
+accountCity = "jaipur"
+let accountState;
+// assigned in the memory 
+// const cant be changed
+// accountId = 2 // not allowed
+// cant be changed bacause const keyword use krliya
+console.log(accountId);
+accountEmail = "jvbk@gmail.com"
+accountPassword = "72415"
+accountCity = "delhi"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+//  variables ko declare krne ke do trike h let or var / var use nhi krte ab
+// { }-- this is scope
+/*
+prefer not to use var because of issue in block scope and functional scope               
+*/
