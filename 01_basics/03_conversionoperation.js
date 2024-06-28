8d4811d55ededde90ded9a3d6c6ca861fe9a38a9
Harshitpapneja <140529258+Harshitpapneja@users.noreply.github.com>
2024-06-29 03:02:27 +0530

    Update 03_conversionoperation.js

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -28,3 +28,42 @@ let stringSomeNumber = String(someNumber)
 console.log(stringSomeNumber)
 console.log(typeof stringSomeNumber)
 
+// jchiowncowecnio "OPERATIONS" CBBCWICNEWINC 
+ 
+let value = 3
+let negValue = -value
+// console.log(negValue );
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**5);
+// console.log(2%3); cryptography
+
+let str1 = "Harshit "
+let str2 = "is handsome"
+let str3 = str1+str2
+console.log(str3)
+ 
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+// string first h to sbhi ko string me treat kiya jaega 
+// normal h to fir phle conversion hoga fir string type wale implement honge   
+
+console.log(+true); 
+console.log(+""); 
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter );
+f
+
+
+
+// https://tc39.es/ecma262/#sec-type-conversion link for type converstion
